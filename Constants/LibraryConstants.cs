975e91192020f2fc965713173a6a502a6b8f0b46
Rekar Anwar <rekarpcpro@gmail.com>
2024-11-21 22:32:50 +0300

    Fix API Base URL

--- Constants/LibraryConstants.cs
+++ Constants/LibraryConstants.cs
@@ -3,6 +3,6 @@
 	internal class LibraryConstants
 	{
 		public const string ClientIPv4AddressHeaderName = "client-ipv4-address";
-		public const string ApiBaseUrl = "https://api-verifyspeed-krqmtmb.up.railway.app";
+		public const string ApiBaseUrl = "https://api.verifyspeed.com";
 	}
 }
