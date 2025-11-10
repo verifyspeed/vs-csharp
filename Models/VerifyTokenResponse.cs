59090a181b6cf3e8a330898ae609d7beaf425bdb
rekar <rekarpc98@gmail.com>
2025-11-10 21:55:57 +0300

    Refactor `VerifyTokenResponse` model structure to wrap namespace and class for consistency.

--- Models/VerifyTokenResponse.cs
+++ Models/VerifyTokenResponse.cs
@@ -1,41 +1,42 @@
 ﻿using System;
 using System.Text.Json.Serialization;
 
-namespace VSCSharp.Models;
-
-/// <summary>
-/// Represents the result of a verification process
-/// </summary>
-public record VerifyTokenResponse
+namespace VSCSharp.Models
 {
 	/// <summary>
-	/// Gets the name of the verification method used.
-	/// For example, "whatsapp-message", or "sms-otp".
+	/// Represents the result of a verification process
 	/// </summary>
-	[JsonPropertyName("methodName")]
-	public string MethodName { get; init; } = null!;
+	public record VerifyTokenResponse
+	{
+		/// <summary>
+		/// Gets the name of the verification method used.
+		/// For example, "whatsapp-message", or "sms-otp".
+		/// </summary>
+		[JsonPropertyName("methodName")]
+		public string MethodName { get; init; } = null!;
 
-	/// <summary>
-	/// Gets the date and time when the verification was completed.
-	/// </summary>
-	[JsonPropertyName("dateOfVerification")]
-	public DateTime DateOfVerification { get; init; }
+		/// <summary>
+		/// Gets the date and time when the verification was completed.
+		/// </summary>
+		[JsonPropertyName("dateOfVerification")]
+		public DateTime DateOfVerification { get; init; }
 
-	/// <summary>
-	/// Gets the verified phone number.
-	/// </summary>
-	[JsonPropertyName("phoneNumber")]
-	public string PhoneNumber { get; init; } = null!;
+		/// <summary>
+		/// Gets the verified phone number.
+		/// </summary>
+		[JsonPropertyName("phoneNumber")]
+		public string PhoneNumber { get; init; } = null!;
 
-	/// <summary>
-	/// Gets the verification key.
-	/// </summary>
-	[JsonPropertyName("verificationKey")]
-	public string VerificationKey { get; init; } = null!;
+		/// <summary>
+		/// Gets the verification key.
+		/// </summary>
+		[JsonPropertyName("verificationKey")]
+		public string VerificationKey { get; init; } = null!;
 
-	/// <summary>
-	/// Indicates whether this is the first time the token has been verified. False means it has been verified before by VerifySpeed.
-	/// </summary>
-	[JsonPropertyName("firstTimeVerified")]
-	public string FirstTimeVerified { get; init; } = null!;
+		/// <summary>
+		/// Indicates whether this is the first time the token has been verified. False means it has been verified before by VerifySpeed.
+		/// </summary>
+		[JsonPropertyName("firstTimeVerified")]
+		public string FirstTimeVerified { get; init; } = null!;
+	}
 }
